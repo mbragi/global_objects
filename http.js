4b09f36ff2232b26e9d9bf551dc769500b34b403
bragi <spontaneousmanji@gmail.com>
2025-06-25 11:04:36 +0100

    Refactor response handling for /gethello endpoint and update TODO comment for dynamic URL responses

--- http.js
+++ http.js
@@ -9,9 +9,9 @@ const PORT = 4000;
 //TODO: READ http methods and status code
 const server = http.createServer((req, res) => {
   if (req.url === "/gethello" && req.method === "GET") {
-   // res.writeHead(200, { "Content-Type": "application/json" });
-   res.writeHead(200, { "Content-Type": "text/plain" });
-   res.end("Hello World")
+    // res.writeHead(200, { "Content-Type": "application/json" });
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World");
   } else {
     res.writeHead(400, { "Content-Type": "text/plain" });
     res.end("Path not implemented");
@@ -22,5 +22,4 @@ server.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
 
-
-//TODO:  IMPROVE API TO GET A REQUEST ON /something and dynamically response using url
+//TODO:  IMPROVE API TO GET A REQUEST ON /something and dynamically response using that url
